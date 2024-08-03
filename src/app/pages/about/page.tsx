114b3ed592f0c062ee7e1ef1bf7ab21146382953
Suraj Mahto <surajmahto255@gmail.com>
2024-08-03 17:02:02 +0530

    fix build

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -93,7 +93,7 @@ const Page = () => {
                             <p className='mb-4'>
                                 The School provides education from UKG to Class VI and is affiliated to the Central Board of Secondary Education (CBSE).Set in an exuberant and lush green environment.
                                 At Aparna Public School, we offer students contemporary and progressive schooling with utmost priority to high quality education. The school is equipped with state –of- the- art facilities and aims to develop students as ambassadors of positive change who will help to build a better tomorrow. With a balanced blend of curricular, co- curricular activities and community service the teaching learning process is enriched.
-                                We want every child to be proficient in academics, technology which is developing rapidly,sociocultural aspects, physically fit and be a class apart. In APARNA PUBLIC SCHOOL we provide an atmosphere which inculcates all sorts of facilities for children to achieve overall  development and doesn't curb to be better than yesterday.
+                                We want every child to be proficient in academics, technology which is developing rapidly,sociocultural aspects, physically fit and be a class apart. In APARNA PUBLIC SCHOOL we provide an atmosphere which inculcates all sorts of facilities for children to achieve overall  development and doesn&apos;t curb to be better than yesterday.
                                 All this is made possible under the guidance of effective leadership, efficient & experienced teachers, unparalleled & unbiased counselling and courteous support staff.
                             </p>
                             <p className="mb-4">
